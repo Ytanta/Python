25296273f537f140bf63cd484c2ac66ca82667f5
Igor <igor>
2023-03-18 14:12:11 +0300

    Задача 04

--- Task10.py
+++ Task10.py
@@ -7,23 +7,24 @@ n = int(input("Введите количество монеток: "))
 if(n<=1):
     print("Закинь еще монеток")
     
-heads_tailst=[random.randint(0, 1) for i in range(int(n))]
-print(heads_tailst)
-head=0
-tailst=0
-for i in range(int(n)):
-    if ((heads_tailst[i]) == 1):
-        head+=1
-    if ((heads_tailst[i]) == 0):
-        tailst+=1
-if(head==0 and tailst==0):
-    print("Вот так удача. Нет лишней работы")
-elif(head==tailst):
-    print("Равное количество")
-elif(head>tailst):
-    print("Перевернуть хвосты")
-else:
-    print("Перевернуть головы")
+else: 
+    heads_tailst=[random.randint(0, 1) for i in range(int(n))]
+    print(heads_tailst)
+    head=0
+    tailst=0
+    for i in range(int(n)):
+        if ((heads_tailst[i]) == 1):
+            head+=1
+        if ((heads_tailst[i]) == 0):
+            tailst+=1
+    if(head==0 and tailst==0):
+        print("Вот так удача. Нет лишней работы")
+    elif(head==tailst):
+        print("Равное количество")
+    elif(head>tailst):
+        print("Перевернуть хвосты")
+    else:
+        print("Перевернуть головы")
 
 
     
